9454a6b8beb2cca66f8110390061e745567b22ef
Hiago Henrique <hiago_h3@hotmail.com>
2022-09-18 12:46:04 -0300

    Adição JavaScript

---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Plankton',
+                partido: 'Chatonildos',
+                fotos:[
+                    {url:'../images/plancton.png', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Sandy Bochechas',
+                partido: 'Fenda do Biquini',
+                fotos:[
+                    {url:'.../images/sandy.png', legenda: 'Deputado Estadual'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '77',
+                nome: 'Bob Esponja',
+                partido: 'Fenda do Biquini',
+                vice: 'Patrick Estrela',
+                fotos:[
+                    {url:'../images/BOB ESPONJA.png', legenda: 'Presidente'},
+                    {url:'../images/PATRICK ESTRELA.webp', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '38',
+                nome: 'Seu Sirigueijo',
+                partido: 'Chatonildos',
+                vice: 'Lula MOlusco',
+                fotos:[
+                    {url:'../images/sirigueijo.png', legenda: 'Presidente'},
+                    {url:'../images/molusco.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
